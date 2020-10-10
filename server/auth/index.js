e021deebfb9d35eb9222b6458a032469dbddadae
Qi Lin <qlin1996@gmail.com>
2020-10-10 19:02:27 -0400

    created cart for new users

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const User = require('../db/models/user')
+const {User, Cart} = require('../db/models')
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
@@ -22,6 +22,7 @@ router.post('/login', async (req, res, next) => {
 router.post('/signup', async (req, res, next) => {
   try {
     const user = await User.create(req.body)
+    await Cart.create({userId: req.body.id})
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
