e8472d7ecd5c257dbab8b475337520d7e44fc640
Qi Lin <qlin1996@gmail.com>
2020-10-20 23:24:42 -0400

    added cart item count

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,18 +1,44 @@
 /* eslint-disable complexity */
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {connect} from 'react-redux'
 import {logout} from '../store'
-import {logoutCart} from '../store/cart'
+import {logoutCart, fetchCartThunk} from '../store/cart'
+import {me} from '../store/user'
 import {NavLink} from 'react-router-dom'
 import Modal from './modal'
 
-const Navbar = ({handleClick, isLoggedIn}) => {
+const Navbar = ({
+  handleClick,
+  isLoggedIn,
+  userId,
+  cart,
+  fetchCart,
+  fetchMe
+}) => {
   const [navClicked, setNavClicked] = useState(false)
   const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(
+    () => {
+      async function fetch() {
+        await fetchMe()
+        if (userId) await fetchCart(userId)
+      }
+      fetch()
+    },
+    [userId]
+  )
 
   const handleClose = () => {
     setIsOpen(false)
   }
+
+  const cartProducts = cart.products || []
+  const cartItemCount = cartProducts.reduce(
+    (accumulator, currentValue) =>
+      accumulator + currentValue.cartItem.cartItemQuantity,
+    0
+  )
 
   return (
     <header>
@@ -81,7 +107,7 @@ const Navbar = ({handleClick, isLoggedIn}) => {
                     setIsOpen(true)
                   }}
                 >
-                  Cart
+                  Cart ({cartItemCount})
                 </NavLink>
               ) : (
                 <NavLink
@@ -89,6 +115,7 @@ const Navbar = ({handleClick, isLoggedIn}) => {
                   to="/cart"
                   onClick={() => setNavClicked(false)}
                 >
+                  <div className="count">{cartItemCount}</div>
                   <i className="fas fa-shopping-cart" />
                 </NavLink>
               )}
@@ -167,19 +194,19 @@ const Navbar = ({handleClick, isLoggedIn}) => {
   )
 }
 
-const mapState = state => {
-  return {
-    isLoggedIn: state.user.id
-  }
-}
+const mapState = state => ({
+  isLoggedIn: state.user.id,
+  userId: state.user.id,
+  cart: state.cart
+})
 
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-      dispatch(logoutCart())
-    }
-  }
-}
+const mapDispatch = dispatch => ({
+  handleClick() {
+    dispatch(logout())
+    dispatch(logoutCart())
+  },
+  fetchCart: userId => dispatch(fetchCartThunk(userId)),
+  fetchMe: () => dispatch(me())
+})
 
 export default connect(mapState, mapDispatch)(Navbar)
